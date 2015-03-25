08c21b51cb87450ef00a8b6e498c4e79a571374c
NylsSs <nilsredoulez@gmail.com>
2015-03-25 10:49:52 +0100

    création des tableaux de caractères

--- enigma/enigma.js
+++ enigma/enigma.js
@@ -1,1 +1,8 @@
- 
+function Enigma {
+	var charsEncrypted = Array(
+			"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y","z", " ", "'", ".", ",",":", ";"
+		);
+	var charsDecrypted = Array(
+			"j", "d", "w", "o", "v", "a", "r", "i", "g", " ", "x", "u", "n", "s", "y", "q", "c", "p", "'", "f", "m", "z", "t", "k", "h", "b", "e", "l", ".", ",",":", ";"
+		);
+}
