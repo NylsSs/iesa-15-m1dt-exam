5faf61e5d7db4bd1d57866681dae5aa7832567a8
NylsSs <nilsredoulez@gmail.com>
2015-03-25 11:32:52 +0100

    ajout du logo

---
+++ site_web/index.php
@@ -0,0 +1,11 @@
+ <!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Le Système de Guanai</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+
+	</body>
+</html>
