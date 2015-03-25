d1b45a00f319508ff4fe5613f0640f6a0703d74a
NylsSs <nilsredoulez@gmail.com>
2015-03-25 12:46:43 +0100

    link entre php et xml

--- site_web/index.php
+++ site_web/index.php
@@ -3,9 +3,21 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Le Système de Guanai</title>
-		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" type="text/css" href="desktop.css" media="screen and (min-width: 481px)">
+		<link rel="stylesheet" type="text/css" href="smartphone.css" media="screen and (min-width: 0px) and (max-width: 480px)">
+		
 	</head>
 	<body>
+		<?php
+			$xml = new SimpleXMLElement('systemSolaire.xml', Null, True);  
+			$planet = $xml->xpath('/planet');
 
+			foreach($planet as $ua) {  
+				echo $ua->image;  
+			}
+			$newsRss = array();
+			$newsRss["title"] = 
+		?>
+		
 	</body>
 </html>
